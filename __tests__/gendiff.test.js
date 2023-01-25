6909d0ab7410fde2232d401e40805af8c7b04336
philologistIsCodingHere <ari.msnkv@gmail.com>
2023-01-25 21:50:27 +0300

    fix linter error

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-undef */
 import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { dirname, path } from 'path';
 import { readFileSync } from 'fs';
-import path from 'path';
 import getDate from '../src/parser.js';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -12,6 +11,6 @@ const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filena
 test('gendiff', () => {
   const file1 = getFixturePath('file1.json');
   const file2 = getFixturePath('file2.json');
-  const expected = readFileSync(getFixturePath('expectedFile.txt'),'utf-8');
+  const expected = readFileSync(getFixturePath('expectedFile.txt'), 'utf-8');
   expect(getDate(file1, file2)).toEqual(expected);
 });
