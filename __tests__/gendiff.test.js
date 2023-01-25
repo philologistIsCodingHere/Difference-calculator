5ab7120f3a554ff4a153d3a80cab55560cf4d92e
philologistIsCodingHere <ari.msnkv@gmail.com>
2023-01-25 21:41:11 +0300

    add __tests__/

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { readFileSync } from 'fs';
+import path from 'path';
+import getDate from '../src/parser.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '__fixtures__', filename);
+
+test('gendiff', () => {
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.json');
+  const expected = readFileSync(getFixturePath('expectedFile.txt'),'utf-8');
+  expect(getDate(file1, file2)).toEqual(expected);
+});
