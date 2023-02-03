ce1f1092bfa8cd0977e8782fdfcd84d258615760
philologistIsCodingHere <ari.msnkv@gmail.com>
2023-02-03 15:24:16 +0300

    edit src/gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -6,9 +6,9 @@ program
   .version('0.0.1')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    console.log(getDate(filepath1, filepath2));
+    console.log(getDate(filepath1, filepath2, program.opts().format));
   });
 
 program.parse();
