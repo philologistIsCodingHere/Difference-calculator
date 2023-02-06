fd048261254eda08e6bc88c666fe176ac6a89afa
philologistIsCodingHere <ari.msnkv@gmail.com>
2023-02-06 23:30:45 +0300

    edit src/parser.js

--- src/parser.js
+++ src/parser.js
@@ -10,12 +10,12 @@ const getDate = (path1, path2, format) => {
   const [cwd1, cwd2] = [process.cwd(path1), process.cwd(path2)];
   const absolutePath1 = path.resolve(cwd1, path1);
   const absolutePath2 = path.resolve(cwd2, path2);
-  const readPath1 = readFileSync(absolutePath1);
-  const readPath2 = readFileSync(absolutePath2);
+  const fileContent1 = readFileSync(absolutePath1);
+  const fileContent2 = readFileSync(absolutePath2);
   if (path.extname(path1) === '.json' && path.extname(path2) === '.json') {
-    result = getDiff(JSON.parse(readPath1), JSON.parse(readPath2));
+    result = getDiff(JSON.parse(fileContent1), JSON.parse(fileContent2));
   } else if (path.extname(path1) === ('.yml' || '.yaml') && path.extname(path2) === ('.yml' || '.yaml')) {
-    result = getDiff(yaml.load(readPath1), yaml.load(readPath2));
+    result = getDiff(yaml.load(fileContent1), yaml.load(fileContent2));
   }
   return formats[format](result);
 };
