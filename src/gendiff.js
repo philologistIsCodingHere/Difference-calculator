a569ab3822b6e94f55aa4d8156205f4e08456ebc
philologistIsCodingHere <ari.msnkv@gmail.com>
2023-02-06 23:51:01 +0300

    edit gendiff.js

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,6 +1,7 @@
-import { program } from 'commander';
+import { Command } from 'commander';
 import getDate from './parser.js';
 
+const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
