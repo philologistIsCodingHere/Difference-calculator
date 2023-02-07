3ced4dbd707db37051b19278ccfaf0380e5d4b97
philologistIsCodingHere <ari.msnkv@gmail.com>
2023-02-07 11:14:35 +0300

    fxied bug

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,8 +1,10 @@
 import getStylish from './stylish.js';
 import getPlain from './plain.js';
 
-export default {
+const formats = {
   stylish: getStylish,
   plain: getPlain,
   json: JSON.stringify,
 };
+
+export default formats;
