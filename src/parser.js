cbf02f077b0bd9e88460bcc2a39be8234c84c06f
philologistIsCodingHere <ari.msnkv@gmail.com>
2023-02-08 21:44:05 +0300

    fixed linter`s errors

--- src/parser.js
+++ src/parser.js
@@ -1,23 +1,27 @@
-import process from 'process';
-import path from 'path';
 import { readFileSync } from 'fs';
-import yaml from 'js-yaml';
-import getDiff from './getdifference.js';
-import formats from './formatters/index.js';
+import { resolve, extname } from 'path';
+import { cwd } from 'process';
+import { load } from 'js-yaml';
 
-const getDate = (path1, path2, format = 'stylish') => {
-  let result;
-  const [cwd1, cwd2] = [process.cwd(path1), process.cwd(path2)];
-  const absolutePath1 = path.resolve(cwd1, path1);
-  const absolutePath2 = path.resolve(cwd2, path2);
-  const fileContent1 = readFileSync(absolutePath1);
-  const fileContent2 = readFileSync(absolutePath2);
-  if (path.extname(path1) === '.json' && path.extname(path2) === '.json') {
-    result = getDiff(JSON.parse(fileContent1), JSON.parse(fileContent2));
-  } else if (path.extname(path1) === ('.yml' || '.yaml') && path.extname(path2) === ('.yml' || '.yaml')) {
-    result = getDiff(yaml.load(fileContent1), yaml.load(fileContent2));
-  }
-  return formats[format](result);
+const parseJSONFiles = (path1, path2) => {
+  const fileContent1 = JSON.parse(readFileSync(path1));
+  const fileContent2 = JSON.parse(readFileSync(path2));
+  return [fileContent1, fileContent2];
 };
 
-export default getDate;
+const parseYAMLFiles = (path1, path2) => {
+  const fileContent1 = load(readFileSync(path1));
+  const fileContent2 = load(readFileSync(path2));
+  return [fileContent1, fileContent2];
+};
+
+export default (path1, path2) => {
+  const FORMATS = {
+    '.yml': parseYAMLFiles,
+    '.yaml': parseYAMLFiles,
+    '.json': parseJSONFiles,
+  };
+  const fullPathFile1 = resolve(cwd(), path1);
+  const fullPathFile2 = resolve(cwd(), path2);
+  return FORMATS[extname(path1)](fullPathFile1, fullPathFile2);
+};
