11de501e1034fb9ec321d7c58d7a7dc6b20f9596
philologistIsCodingHere <ari.msnkv@gmail.com>
2023-02-08 21:41:17 +0300

    edit tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,7 +2,7 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { readFileSync } from 'fs';
-import getDate from '../src/parser.js';
+import gendiff from '../src/getdifference.js';
 
 const JSON_FILE_1 = 'file1.json';
 const JSON_FILE_2 = 'file2.json';
@@ -20,9 +20,9 @@ test('json files', () => {
   const expectedPlain = readFile('expectedFilePlain.txt');
   const expectedJson = readFile('expectedFileJson.txt');
   const [jsonFile1, jsonFile2] = [getFixturePath(JSON_FILE_1), getFixturePath(JSON_FILE_2)];
-  expect(getDate(jsonFile1, jsonFile2, 'stylish')).toEqual(expectedStylish);
-  expect(getDate(jsonFile1, jsonFile2, 'plain')).toEqual(expectedPlain);
-  expect(getDate(jsonFile1, jsonFile2, 'json')).toEqual(expectedJson);
+  expect(gendiff(jsonFile1, jsonFile2, 'stylish')).toEqual(expectedStylish);
+  expect(gendiff(jsonFile1, jsonFile2, 'plain')).toEqual(expectedPlain);
+  expect(gendiff(jsonFile1, jsonFile2, 'json')).toEqual(expectedJson);
 });
 
 test('yml files', () => {
@@ -30,7 +30,7 @@ test('yml files', () => {
   const expectedPlain = readFile('expectedFilePlain.txt');
   const expectedJson = readFile('expectedFileJson.txt');
   const [ymlFile1, ymlFile2] = [getFixturePath(YML_FILE_1), getFixturePath(YML_FILE_2)];
-  expect(getDate(ymlFile1, ymlFile2, 'stylish')).toEqual(expectedStylish);
-  expect(getDate(ymlFile1, ymlFile2, 'plain')).toEqual(expectedPlain);
-  expect(getDate(ymlFile1, ymlFile2, 'json')).toEqual(expectedJson);
+  expect(gendiff(ymlFile1, ymlFile2, 'stylish')).toEqual(expectedStylish);
+  expect(gendiff(ymlFile1, ymlFile2, 'plain')).toEqual(expectedPlain);
+  expect(gendiff(ymlFile1, ymlFile2, 'json')).toEqual(expectedJson);
 });
