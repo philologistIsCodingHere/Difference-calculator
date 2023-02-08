5100e560a7638720c1aa2b75e962bdba79bc325e
philologistIsCodingHere <ari.msnkv@gmail.com>
2023-02-08 21:42:56 +0300

    fixed linter`s errors

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -12,32 +12,30 @@ const getValue = (value) => {
   return value;
 };
 
-const getNode = (key, node, path = []) => {
-  path.push(key);
+const getNode = (node, path = []) => {
+  const { key, type, value } = node;
+  const newPath = [...path, key];
   const result = [];
-  const [type, value] = [node.typeValue, node.valueItem];
   if (type === 'added') {
-    result.push(`Property '${path.join('.')}' was added with value: ${getValue(value)}`);
+    return [...result, `Property '${newPath.join('.')}' was added with value: ${getValue(value)}`];
   }
   if (type === 'removed') {
-    result.push(`Property '${path.join('.')}' was removed`);
+    return [...result, `Property '${newPath.join('.')}' was removed`];
   }
   if (type === 'changed') {
     const [value1, value2] = value;
-    result.push(`Property '${path.join('.')}' was updated. From ${getValue(value1)} to ${getValue(value2)}`);
+    return [...result, `Property '${newPath.join('.')}' was updated. From ${getValue(value1)} to ${getValue(value2)}`];
   }
   if (type === 'nested') {
-    const keys = Object.keys(value);
-    const nodesString = keys.flatMap((item) => getNode(item, value[item], path));
-    result.push(nodesString.join(LINE_BREAK));
+    const nodesString = value.flatMap((item) => getNode(item, newPath));
+    return [...result, nodesString.join(LINE_BREAK)];
   }
-  path.pop();
+  newPath.filter((item) => !!item.length).join('\n');
   return result;
 };
 
 const getPlain = (tree) => {
-  const keys = Object.keys(tree);
-  const plainFormat = keys.flatMap((key) => getNode(key, tree[key]));
+  const plainFormat = tree.flatMap((node) => getNode(node));
   return plainFormat.join(LINE_BREAK);
 };
 
